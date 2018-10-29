304c69f9035c78a6a870e80eb81cf641e2fe7842
deadmau6 <joepop16@gmail.com>
2018-10-28 18:49:55 -0700

    Updated Fourier to be more accurate

--- Fourier/fourier.py
+++ Fourier/fourier.py
@@ -1,10 +1,10 @@
 import numpy as np
+import math
 
 class Fourier(object):
     """docstring for Fourier"""
-    def __init__(self, time):
-        self.rate = -1 * 2 * np.pi
-        self.time = time
+    def __init__(self):
+        self.rate = -2j * np.pi
 
     def Series(self):
         pass
@@ -13,10 +13,47 @@ class Fourier(object):
         pass
 
     def Discrete(self, x):
-        N = len(x)
-        k = np.arange(N)
-        e = np.cos(self.rate * k * self.time / N) + 1j*np.sin(self.rate * k * self.time / N)
-        return (x * e)
+        N = x.shape[0]
+        n = np.arange(N)
+        k = n.reshape((N, 1))
+        M = np.exp(self.rate * k * n / N)
+        return np.dot(M, x)
+
+    def Fast(self, x, vect=True):
+        # Size of x.
+        N = x.shape[0]
+        # Find the nearest power of 2.
+        lim = 2 ** int(math.log2(N))
+        if vect:
+            return self._fast_vectorization(x[:lim])
+        else:
+            return self._fast_recursion(x[:lim])
+
+    def _fast_vectorization(self, x):
+        N = min(x.shape[0], 16)
+
+        n = np.arange(N)
+        k = n[:, None]
+        M = np.exp(self.rate * k * n / N)
+        X = np.dot(M, x.reshape((N, -1)))
+
+        while X.shape[0] < N:
+            X_even = X[:, :X.shape[1] / 2]
+            X_odd = X[:, X.shape[1] / 2:]
+            factor = np.exp(-1j* np.pi * np.arange(X.shape[0]) / X.shape[0])[:, None]
+            X = np.vstack([X_even + factor * X_odd, X_even - factor * X_odd])
+
+        return X.ravel()
+
+    def _fast_recursion(self, x):
+        N = x.shape[0]
+        if N <= 8:
+            return self.Discrete(x)
+        else:
+            x_even = self._fast_recursion(x[::2])
+            x_odd = self._fast_recursion(x[1::2])
+            factor = np.exp(self.rate * np.arange(N) / N)
+            return np.concatenate([x_even + factor[:N >> 1] * x_odd, x_even + factor[N >> 1:] * x_odd])
 
     def complex_to_linear(self, x, d=3):
         return np.around(np.sqrt((x.real ** 2) + (x.imag ** 2)), d)
--- Fourier/plotter.py
+++ Fourier/plotter.py
@@ -2,20 +2,23 @@ import matplotlib.pyplot as pt
 from fourier import Fourier
 import numpy as np
 
-N = 2.0 * np.pi
-dt = 0.001
-t = np.arange(0.0, N, dt)
+t = np.arange(0.0, 8, 0.01)
 
-jf = Fourier(t)
-
+print(t.shape[0])
 
 s1 = np.sin(2 * np.pi * t)
-s2 = np.cos(4 * np.pi * t)
-s3 = s1 + s2
+s2 = np.cos(4 * np.pi * t) + s1
+s3 = np.random.random(128)
 
-f_s1 = jf.Discrete(s1)
-f_s2 = jf.Discrete(s2)
-f_s3 = jf.Discrete(s3)
+jf = Fourier()
+
+#f_s1 = jf.Fast(s1)
+#f_s2 = jf.Fast(s2)
+#f_s3 = jf.Fast(s3)
+f_s1 = np.fft.fft(s1)
+#f_s2 = np.fft.fft(s2)
+f_s2 = jf.Fast(s2, vect=False)
+f_s3 = np.fft.fft(s3)
 
 m_s1 = jf.complex_to_linear(f_s1)
 m_s2 = jf.complex_to_linear(f_s2)
@@ -27,7 +30,7 @@ ax[0, 0].plot(t, s1)
 
 ax[0, 1].plot(t, s2)
 
-ax[0, 2].plot(t, s3)
+ax[0, 2].plot(np.arange(s3.shape[0]), s3)
 
 ax[1, 0].plot(f_s1.real, f_s1.imag)
 ax[1, 0].set_aspect('equal', 'box')
@@ -38,10 +41,7 @@ ax[1, 1].set_aspect('equal', 'box')
 ax[1, 2].plot(f_s3.real, f_s3.imag)
 ax[1, 2].set_aspect('equal', 'box')
 
-ax[2, 0].plot(t, m_s1)
-
-ax[2, 1].plot(t, m_s2)
-
-ax[2, 2].plot(t, m_s3)
-
+ax[2, 0].plot(np.arange(m_s1.shape[0]), m_s1)
+ax[2, 1].plot(np.arange(m_s2.shape[0]), m_s2)
+ax[2, 2].plot(np.arange(m_s3.shape[0]), m_s3)
 pt.show()
