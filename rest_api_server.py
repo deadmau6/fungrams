8d81c8183b6fe753bb4f7cc88d94fce3e7ff8e5e
JoePagani <josephepagani@gmail.com>
2018-11-27 16:46:56 -0800

    Added a basic rest api server

---
+++ rest_api_server.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from LogMonitor import Scanner, MongoParser, NodeParser
+import json
+
+STREAM_FILE = '/var/log/mongodb/mongod.log'
+
+app = Flask(__name__)
+api = Api(app)
+
+class User(Resource):
+    """docstring for User"""
+    def get(self):
+        return {"message": "hello new user!"}
+
+class Logger(Resource):
+    """docstring for logger"""
+    def __init__(self):
+        super().__init__()
+        self.scanner = Scanner()
+        self.mongo = MongoParser()
+
+    def get(self):
+        logs = []
+        with open(STREAM_FILE, 'r') as f:
+            for entry in self.mongo.parse(self.scanner.tokenize(f.read())):
+                logs.append(entry.toJSON())
+        return logs
+
+api.add_resource(User, '/')
+
+api.add_resource(Logger, '/logs')
+
+if __name__ =='__main__':
+    app.run(debug=True)
+        
