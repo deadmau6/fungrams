e6f5683215b12918029d9b8a427670ac8d38c93a
JoePagani <josephepagani@gmail.com>
2019-02-06 12:04:49 -0800

    **Added Longest common subsequence**

--- DP/dynamic.py
+++ DP/dynamic.py
@@ -1,3 +1,4 @@
+
 class Dynamic:
     
     @staticmethod
@@ -31,8 +32,35 @@ class Dynamic:
             return 0
 
         return Dynamic.fibonacci(N-1) + Dynamic.fibonacci(N-2)
-            
 
+    @staticmethod
+    def LCS(X, Y, m, n):
+        """ Given two character sequences X and Y, find the longest common subsequence.
+        Note that comparison between characters in the sequences is in fact case sensitive!
+        """
+        L = [[0]*(n+1) for i in range(m+1)]
+        for i in range(m+1):
+            for j in range(n+1):
+                if i == 0 or j == 0:
+                    L[i][j] = 0
+                elif X[i-1] == Y[j-1]:
+                    L[i][j] = L[i-1][j-1] + 1
+                else:
+                    L[i][j] = max(L[i-1][j], L[i][j-1])
+        
+        lcs = []
+        i = m
+        j = n
+        while i > 0 and j > 0:
+            if X[i-1] == Y[j-1]:
+                lcs.append(X[i-1])
+                i-=1
+                j-=1
+            elif L[i-1][j] > L[i][j-1]:
+                i-=1
+            else:
+                j-=1
+        return ''.join(lcs[::-1])
 
 
     def start(self, args):
@@ -45,5 +73,10 @@ class Dynamic:
             print(Dynamic.sum_subset(n_sum, list(subset)))
         elif args.fib:
             print(Dynamic.fibonacci(args.fib))
+        elif args.lcs:
+            s1 = list(args.lcs[0])
+            s2 = list(args.lcs[1])
+            subseq = Dynamic.LCS(s1, s2, len(s1), len(s2))
+            print(f"Found LCS: {subseq}\nLCS Length: {len(subseq)}")
         else:
-            print('done')
+            pass
