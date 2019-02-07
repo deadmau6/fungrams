01a3bde88cf25ec134bcf766720250fd5725b2cb
JoePagani <josephepagani@gmail.com>
2019-02-07 10:22:11 -0800

    added a 'random programs' class

---
+++ Things/things.py
@@ -0,0 +1,46 @@
+import random
+
+class Things:
+    """Things is a random assortment of programs, utilities, and games."""
+
+    @staticmethod
+    def pennys_game():
+        """Penny's Game is sequence generating game between two players in which,
+        player A selects a binary sequence of length 3 and then player B picks a
+        different sequence and whose sequence appears first wins. For simplicity
+        this version converts the binary sequence to an integer value so both
+        players pick a number from 0 to 7.
+        """
+        choice = {
+            '111' : 3,
+            '110' : 3,
+            '101' : 6,
+            '100' : 6,
+            '011' : 1,
+            '010' : 1,
+            '001' : 4,
+            '000' : 4
+        }
+        print("Guess a number from 0 - 7:")
+        usr_msg = input()
+        usr_guess = int(usr_msg, 10)
+        usr_guess_bits = format(usr_guess, '0>3b')
+        my_guess = choice[usr_guess_bits]
+        print(f"Okay my guess is {my_guess}")
+        while True:
+            try:
+                r = random.getrandbits(3)
+                print(r)
+                if r == usr_guess:
+                    print("You Won!")
+                    break
+                if r == my_guess:
+                    print("I Won!")
+                    break
+            except KeyboardInterrupt:
+                print('Exiting...')
+                break
+
+
+    def start(self):
+        Things.pennys_game()
